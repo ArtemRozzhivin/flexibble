62962755b11bcd0fc70fc004fea11733d2ef4ada
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-09-16 17:13:58 +0300

    create layout template

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { footerLinks } from '@constants';
+
+type FooterColumnProps = {
+  title: string;
+  links: string[];
+};
+
+const FooterColumn = ({ title, links }: FooterColumnProps) => (
+  <div className='footer_column'>
+    {title && <h3 className='font-semibold'>{title}</h3>}
+
+    <ul className='flex flex-col gap-2'>
+      {links.map((link) => (
+        <li key={link}>
+          <Link href='/'>
+            <p className='font-light'>{link}</p>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Link href='/'>
+            <Image src='/logo.svg' width={115} height={43} alt='Flexibble' />
+          </Link>
+
+          <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+            Flexibble is the world's leading community for creatives to share, grow, and get hired.
+          </p>
+        </div>
+
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+
+          <div className='flex flex-col flex-1 gap-5'>
+            <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links} />
+            <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links} />
+
+          <div className='flex flex-col flex-1 gap-5'>
+            <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links} />
+            <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links} />
+        </div>
+      </div>
+
+      <div className='footer_copyright flexBetween pt-5 border-t-2 border-t-light-white-500'>
+        <p className='text-sm font-light'>@ 2023 Flexibble. All rights reserved.</p>
+
+        <p className='text-sm font-light'>
+          <span className='font-semibold'>8,912</span> projects submited
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,40 @@
+import { NavLinks } from '@constants';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import AuthProviders from './AuthProviders';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const session = {};
+
+  return (
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image src='/logo.svg' width={115} height={43} alt='Flexibble' />
+        </Link>
+
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <li key={link.key}>
+              <Link href={link.href}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        {session ? (
+          <div className='flexCenter gap-5'>
+            UserPhoto
+            <Link href='/create-project'>ShareWork</Link>
+          </div>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
