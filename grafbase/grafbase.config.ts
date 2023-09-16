541897c457a0b70a03463a540b2620fcba6eaed9
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-09-16 18:13:02 +0300

    create grafBase config

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,28 @@
+import { g, auth, config } from '@grafbase/sdk';
+
+const User = g.model('User', {
+  name: g.string().length({ min: 3, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  gitHubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g
+    .relation(() => Project)
+    .list()
+    .optional(),
+});
+
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  gitHubUrl: g.url(),
+  liveSiteUrl: g.url(),
+  category: g.string().search(),
+  image: g.url(),
+  createdBy: g.relation(() => User),
+});
+
+export default config({
+  schema: g,
+});
